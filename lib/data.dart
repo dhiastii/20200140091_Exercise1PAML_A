79c59329f8b9f4cb6d666a78dcefec69f5105102
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-04-02 04:47:01 +0700

    memasukkan data data dari pokedex

--- lib/data.dart
+++ lib/data.dart
@@ -1,11 +1,200 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'register.dart';
+import 'package:flutter/material.dart';
 
-class DataPage extends StatelessWidget {
-  const DataPage({super.key});
+class DataPage extends StatefulWidget {
+  const DataPage({super.key, required this.name});
+
+  final String name;
+
+  @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  String? searchText = '';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Container(
+                margin: EdgeInsets.only(top: 20, left: 20),
+                alignment: Alignment.topLeft,
+                child: RichText(
+                    text: TextSpan(
+                  text: 'Hello, ' + widget.name,
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ))),
+            Container(width: 250),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+              child: Image.asset(
+                "assets/images/logoreg.png",
+              ),
+            ),
+          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Container(
+                margin: EdgeInsets.only(top: 10, left: 20),
+                alignment: Alignment.topLeft,
+                child: RichText(
+                    text: TextSpan(
+                  text: 'Pokedex',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(236, 243, 192, 26)),
+                ))),
+          ]),
+          Column(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(top: 10, left: 15, right: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search Pokemon',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      contentPadding: EdgeInsets.all(15.0),
+                      filled: true,
+                      fillColor: Colors.grey.shade300,
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none),
+                    ),
+                    style: TextStyle(color: Colors.black),
+                    onChanged: (value) {
+                      setState(() {
+                        searchText = value;
+                      });
+                    },
+                  ))
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/bulbasaur.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 12.0),
+              Expanded(
+                child: Text(
+                  'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/ivysaur.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 12.0),
+              Expanded(
+                child: Text(
+                  'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/venusaur.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 12.0),
+              Expanded(
+                child: Text(
+                  'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/charmander.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(width: 12.0),
+              Expanded(
+                child: Text(
+                  'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
