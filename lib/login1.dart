4c9b04da1c57d1577923c7f98181d65a3a783b0d
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-04-01 18:23:45 +0700

    mengubah login page jadi login1 page

---
+++ lib/login1.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'register.dart';
+import 'package:flutter/material.dart';
+
+class loginpage extends StatefulWidget {
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  final _formfield = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+          child: Form(
+            key: _formfield,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 40),
+                  Image.asset(
+                    "assets/images/loginpageatas.png",
+                  ),
+                  Image.asset("assets/images/loginpagetengah.png"),
+                  SizedBox(height: 50),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Username",
+                      hintText: "Enter your username",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Username cannot be empty";
+                      }
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: passController,
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Enter your Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffix: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: Icon(passToggle
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Password cannot be empty";
+                      } else if (passController.text.length < 6) {
+                        return "Password must be at least 6 characters";
+                      }
+                    },
+                  ),
+                  SizedBox(height: 40),
+                  InkWell(
+                    onTap: () {
+                      //validation
+                      if (_formfield.currentState!.validate()) {
+                        print("Success!");
+                        emailController.clear();
+                        passController.clear();
+                      }
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 30.0),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 14, 61, 163),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't Have Account?",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Add your onPressed function here
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()));
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
