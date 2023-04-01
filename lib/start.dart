d1273b1882455b7337cff07f98f8e3155171feee
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-04-01 13:20:19 +0700

    membuat tampilan dan button pada start screen

--- lib/start.dart
+++ lib/start.dart
@@ -1,11 +1,61 @@
+import 'package:exepaml1/login.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class splashscreen extends StatefulWidget {
+  const splashscreen({super.key});
 
   @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(),
+            SizedBox(height: 400.0),
+            Container(
+              height: 50.0,
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 100.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your onPressed function here
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 14, 61, 163),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
